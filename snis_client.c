a7c53c76c1f9edfeb11ac84b3b981f5a97aa3709
Stephen M. Cameron <stephenmcameron@gmail.com>
2012-11-17 19:54:27 -0600

    factor out weapons ui stuff

--- snis_client.c
+++ snis_client.c
@@ -2400,15 +2400,6 @@ static void show_navigation(GtkWidget *w)
 	draw_all_the_sparks(w, o);
 }
 
-static void load_torpedo_button_pressed(__attribute__((unused)) void *notused)
-{
-	printf("LOAD TORPEDO\n");
-}
-
-static void fire_torpedo_button_pressed(__attribute__((unused)) void *notused)
-{
-	do_torpedo();
-}
 
 typedef void (*button_function)(void *cookie);
 
@@ -2420,6 +2411,21 @@ struct button {
 	button_function bf;
 	void *cookie;
 };
+
+struct weapons_ui {
+	struct button fire_torpedo, load_torpedo;
+} weapons;
+
+static void load_torpedo_button_pressed(__attribute__((unused)) void *notused)
+{
+	printf("LOAD TORPEDO\n");
+}
+
+static void fire_torpedo_button_pressed(__attribute__((unused)) void *notused)
+{
+	do_torpedo();
+}
+
 static void button_init(struct button *b, int x, int y, int width, int height, char *label,
 			GdkColor *color, int font, button_function bf, void *cookie);
 static void add_button(struct button *b);
@@ -2431,15 +2437,14 @@ static void show_weapons(GtkWidget *w)
 	int rx, ry, rw, rh, cx, cy;
 	int r;
 	static int initialized = 0;
-	static struct button fire_torpedo, load_torpedo;
 
 	if (!initialized) {
-		button_init(&load_torpedo, 550, 200, 200, 30, "LOAD TORPEDO", &huex[BLUE],
+		button_init(&weapons.load_torpedo, 550, 200, 200, 30, "LOAD TORPEDO", &huex[BLUE],
 				TINY_FONT, load_torpedo_button_pressed, NULL);
-		button_init(&fire_torpedo, 550, 250, 200, 30, "FIRE TORPEDO", &huex[BLUE],
+		button_init(&weapons.fire_torpedo, 550, 250, 200, 30, "FIRE TORPEDO", &huex[BLUE],
 				TINY_FONT, fire_torpedo_button_pressed, NULL);
-		add_button(&load_torpedo);
-		add_button(&fire_torpedo);
+		add_button(&weapons.load_torpedo);
+		add_button(&weapons.fire_torpedo);
 		initialized = 1;
 	}
 
