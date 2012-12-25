9d3a2e552985b2327a10f39283daaea40b6fdbfe
Stephen M. Cameron <stephenmcameron@gmail.com>
2012-12-25 10:05:15 -0600

    Add snis_gauge.[ch]

---
+++ snis_gauge.h
@@ -0,0 +1,26 @@
+#ifndef SNIS_GAUGE_H__
+#define SNIS_GAUGE_H__
+
+#ifdef DEFINE_SNIS_GAUGE_GLOBALS
+#define GLOBAL
+#else
+#define GLOBAL extern
+#endif
+
+typedef double (*gauge_monitor_function)(void);
+
+struct gauge;
+
+GLOBAL void gauge_add_needle(struct gauge *g, gauge_monitor_function sample, int color);
+
+GLOBAL void gauge_init(struct gauge *g, 
+			int x, int y, int r, double r1, double r2,
+			double start_angle, double angular_range,
+			int needle_color, int dial_color, int ndivs, char *title,
+			gauge_monitor_function gmf);
+GLOBAL void draw_gauge_needle(GdkDrawable *drawable, GdkGC *gc,
+		gint x, gint y, gint r, double a);
+
+GLOBAL void gauge_draw(GtkWidget *w, GdkGC *gc, struct gauge *g);
+
+#endif
