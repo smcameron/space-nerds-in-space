2f21259ceac68a445fe04182dbe95ee7cf13ccc4
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-05-02 23:02:32 -0500

    refactor spin points

--- snis_client.c
+++ snis_client.c
@@ -719,15 +719,51 @@ void scale_points(struct my_point_t *points, int npoints,
 	} 
 }
 
+void rotate_points(struct my_point_t *points, int npoints,
+			struct my_point_t *rotated_points, double angle,
+			int originx, int originy)
+{
+	int i;
+	double startx, starty, start_angle, new_angle, newx, newy;
+	double magnitude, diff;
+
+	for (i = 0; i < npoints; i++) {
+
+		if (points[i].x == LINE_BREAK ||
+			points[i].x == COLOR_CHANGE) {
+			rotated_points[i].x = points[i].x;
+			rotated_points[i].y = points[i].y;
+			continue;
+		}
+		
+		startx = (double) (points[i].x - originx);
+		starty = (double) (points[i].y - originy);
+		magnitude = sqrt(startx * startx + starty * starty);
+		diff = (startx - 0);
+		if (diff < 0 && diff > -0.00001)
+			start_angle = 0.0;
+		else if (diff > 00 && diff < 0.00001)
+			start_angle = M_PI;
+		else
+			start_angle = atan2(starty, startx);
+		new_angle = start_angle + angle;
+		newx = cos(new_angle) * magnitude + originx;
+		newy = sin(new_angle) * magnitude + originy;
+		rotated_points[i].x = newx;
+		rotated_points[i].y = newy;
+		/* printf("s=%f,%f, e=%f,%f, angle = %f/%f\n", 
+			startx, starty, newx, newy, 
+			start_angle * 180/M_PI, new_angle * 360.0 / (2.0 * M_PI));  */
+	}
+}
+
 void spin_points(struct my_point_t *points, int npoints, 
 	struct my_point_t **spun_points, int nangles,
 	int originx, int originy)
 {
-	int i, j;
+	int i;
 	double angle;
 	double angle_inc;
-	double startx, starty, start_angle, new_angle, newx, newy;
-	double magnitude, diff;
 
 	*spun_points = (struct my_point_t *) 
 		malloc(sizeof(*spun_points) * npoints * nangles);
@@ -740,34 +776,8 @@ void spin_points(struct my_point_t *points, int npoints,
 	for (i = 0; i < nangles; i++) {
 		angle = angle_inc * (double) i;
 		/* printf("Rotation angle = %f\n", angle * 180.0 / M_PI); */
-		for (j = 0; j < npoints; j++) {
-
-			if (points[j].x == LINE_BREAK ||
-				points[j].x == COLOR_CHANGE) {
-				(*spun_points)[i*npoints + j].x = points[j].x;
-				(*spun_points)[i*npoints + j].y = points[j].y;
-				continue;
-			}
-			
-			startx = (double) (points[j].x - originx);
-			starty = (double) (points[j].y - originy);
-			magnitude = sqrt(startx*startx + starty*starty);
-			diff = (startx - 0);
-			if (diff < 0 && diff > -0.00001)
-				start_angle = 0.0;
-			else if (diff > 00 && diff < 0.00001)
-				start_angle = M_PI;
-			else
-				start_angle = atan2(starty, startx);
-			new_angle = start_angle + angle;
-			newx = cos(new_angle) * magnitude + originx;
-			newy = sin(new_angle) * magnitude + originy;
-			(*spun_points)[i*npoints + j].x = newx;
-			(*spun_points)[i*npoints + j].y = newy;
-			/* printf("s=%f,%f, e=%f,%f, angle = %f/%f\n", 
-				startx, starty, newx, newy, 
-				start_angle * 180/M_PI, new_angle * 360.0 / (2.0 * M_PI));  */
-		}
+		rotate_points(points, npoints, &(*spun_points)[i * npoints],
+				angle, originx, originy);
 	} 
 }
 
