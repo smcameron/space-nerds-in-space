e24ba99d8b706725e605886ab77af23af41b84c7
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-06-08 07:47:23 -0500

    remove magic asteroid number

--- snis_client.c
+++ snis_client.c
@@ -190,7 +190,7 @@ double cosine[361];
 struct mesh *torpedo_mesh;
 struct mesh *laser_mesh;
 struct mesh *planet_mesh;
-struct mesh *asteroid_mesh[4];
+struct mesh *asteroid_mesh[NASTEROID_MODELS];
 struct mesh *starbase_mesh;
 struct mesh *ship_mesh;
 struct mesh *freighter_mesh;
@@ -684,7 +684,8 @@ static int update_asteroid(uint32_t id, double x, double y)
 
 	i = lookup_object_by_id(id);
 	if (i < 0) {
-		e = add_entity(asteroid_mesh[snis_randn(4)], x, 0, -y, ASTEROID_COLOR);
+		e = add_entity(asteroid_mesh[snis_randn(NASTEROID_MODELS)],
+					x, 0, -y, ASTEROID_COLOR);
 		i = add_generic_object(id, x, y, 0.0, 0.0, 0.0, OBJTYPE_ASTEROID, 1, e);
 		if (i < 0)
 			return i;
@@ -6343,18 +6344,25 @@ static void setup_joystick(GtkWidget *window)
 static void init_meshes(void)
 {
 #if 1
+	int i;
+
 	ship_mesh = read_stl_file("spaceship.stl");
 	torpedo_mesh = read_stl_file("torpedo.stl");
 	laser_mesh = read_stl_file("laser.stl");
 	planet_mesh = read_stl_file("planet.stl");
-	asteroid_mesh[0] = read_stl_file("asteroid.stl");
-	asteroid_mesh[1] = read_stl_file("asteroid2.stl");
-	asteroid_mesh[2] = read_stl_file("asteroid3.stl");
-	asteroid_mesh[3] = read_stl_file("asteroid4.stl");
-	distort_mesh(asteroid_mesh[0], 0.10);
-	distort_mesh(asteroid_mesh[1], 0.10);
-	distort_mesh(asteroid_mesh[2], 0.10);
-	distort_mesh(asteroid_mesh[3], 0.10);
+
+	for (i = 0; i < NASTEROID_MODELS; i++) {
+		char filename[100];
+
+		if (i == 0)
+			sprintf(filename, "asteroid.stl");
+		else
+			sprintf(filename, "asteroid%d.stl", i + 1);
+		printf("reading '%s'\n", filename);
+		asteroid_mesh[i] = read_stl_file(filename);
+		distort_mesh(asteroid_mesh[i], 0.10);
+	}
+
 	starbase_mesh = read_stl_file("starbase.stl");
 	freighter_mesh = read_stl_file("freighter.stl");
 	cruiser_mesh = read_stl_file("cruiser.stl");
