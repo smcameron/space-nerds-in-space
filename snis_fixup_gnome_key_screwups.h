8a76a9d6ca97420183e58bc023c8d33888ee3a7c
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-11-06 21:39:54 -0600

    fix some gdk key defines for Fedora
    Thanks to Christopher Voltz for identifying these

--- snis_fixup_gnome_key_screwups.h
+++ snis_fixup_gnome_key_screwups.h
@@ -17,3 +17,32 @@
 #ifndef GDK_KEY_BackSpace
 #define GDK_KEY_BackSpace GDK_BackSpace
 #endif
+
+#ifndef GDK_KEY_Tab
+#define GDK_KEY_Tab GDK_Tab
+#endif
+
+#ifndef GDK_KEY_plus
+#define GDK_KEY_plus GDK_plus
+#endif
+
+#ifndef GDK_KEY_equal
+#define GDK_KEY_equal GDK_equal
+#endif
+
+#ifndef GDK_KEY_minus
+#define GDK_KEY_minus GDK_minus
+#endif
+
+#ifndef GDK_KEY_KP_Add
+#define GDK_KEY_KP_Add GDK_KP_Add
+#endif
+
+#ifndef GDK_KEY_KP_Subtract
+#define GDK_KEY_KP_Subtract GDK_KP_Subtract
+#endif
+
+#ifndef GDK_KEY_Return
+#define GDK_KEY_Return GDK_Return
+#endif
+
