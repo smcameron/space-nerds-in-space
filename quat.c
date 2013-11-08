7d8d0a2161f9d4a14eb6fe1aec32154ee9eeff89
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-11-08 07:08:10 -0600

    remove some quat related debug code

--- quat.c
+++ quat.c
@@ -131,7 +131,6 @@ void quat_to_heading_mark(const union quat *q, double *heading, double *mark)
 	quat_rot_vec_self(&dir, q);
 
 	*heading = normalize_euler_0_2pi(atan2(-dir.v.z,dir.v.x));
-vec3_print("quat_to_heading_mark=",&dir);
 	*mark = copysign(acos(sqrt(dir.v.x*dir.v.x + dir.v.z*dir.v.z)),dir.v.y);
 }
 
