014f770762bb658846d7bfa3d15ed171bb0d8c3c
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-12-03 10:08:21 -0600

    add faction hostility matrix

--- snis_faction.c
+++ snis_faction.c
@@ -14,6 +14,8 @@
 struct faction {
 	char *name;
 	union vec3 center;
+	float hostility[MAX_FACTIONS];
+#define DEFAULT_FACTION_HOSTILITY 0.03f
 };
 
 static struct faction *fact;
@@ -24,6 +26,53 @@ int nfactions(void)
 	return nfacts;
 }
 
+static int lookup_faction(char *name)
+{
+	int i;
+
+	for (i = 0; i < nfacts; i++)
+		if (strcmp(name, fact[i].name) == 0)
+			return i;
+	return -1;
+}
+
+static int process_hostility(char *line)
+{
+	int n;
+	char a[255], b[255];
+	int hostility;
+	int f1, f2;
+
+	n = sscanf(line, "hostility %[A-Za-z] %[A-Za-z] %d", a, b, &hostility);
+	if (n != 3)
+		return -1;
+	f1 = lookup_faction(a);
+	if (f1 < 0) {
+		fprintf(stderr, "Bad faction '%s'\n", a);
+		return -1;
+	}
+	f2 = lookup_faction(b);
+	if (f1 < 0) {
+		fprintf(stderr, "Bad faction '%s'\n", b);
+		return -1;
+	}
+	fact[f1].hostility[f2] = (float) hostility / 100.0f;
+	fact[f2].hostility[f1] = (float) hostility / 100.0f;
+	return 0;
+}
+
+static void initialize_faction_hostility()
+{
+	int i, j;
+
+	for (i = 0; i < MAX_FACTIONS; i++)
+		for (j = 0; j < MAX_FACTIONS; j++)
+			if (i == j)
+				fact[i].hostility[j] = 0;
+			else
+				fact[i].hostility[j] = DEFAULT_FACTION_HOSTILITY;
+}
+			
 int snis_read_factions(char *filename)
 {
 	FILE *f;
@@ -44,14 +93,26 @@ int snis_read_factions(char *filename)
 		return -1;
 	}
 
+	initialize_faction_hostility();
+
 	while (!feof(f) && n < MAX_FACTIONS) {
 		c = fgets(line, sizeof(line) - 1, f);
 		if (!c)
 			break;
+		line[strlen(line) - 1] = '\0';
 		linecount++;
 
 		if (line[0] == '#') /* skip comments */
 			continue;
+	
+		if (strncmp(line, "hostility ", 10) == 0) {
+			if (process_hostility(line)) {
+				fprintf(stderr, "bad faction hostility %s:%d\n",
+					filename, linecount);
+				return -1;
+			}
+			continue;
+		}
 
 		fact[n].name = malloc(strlen(line) + 1);
 		if (!fact[n].name) {
@@ -62,6 +123,7 @@ int snis_read_factions(char *filename)
 		c = fgets(line, sizeof(line) - 1, f);
 		if (!c)
 			break;
+		line[strlen(line) - 1] = '\0';
 		r = sscanf(line, "%d %d %d", &x, &y, &z);
 		if (r != 3) {
 			fprintf(stderr, "bad line '%s' at %s:%d\n", line, filename, linecount);
@@ -71,9 +133,9 @@ int snis_read_factions(char *filename)
 		fact[n].center.v.x = (float) x;
 		fact[n].center.v.y = (float) y;
 		fact[n].center.v.z = (float) z;
+		nfacts++;
 		n++;
 	}
-	nfacts = n;
 	return 0;
 }
 
@@ -114,5 +176,12 @@ int nearest_faction(union vec3 v)
 		}
 	}
 	return winner;
+}
+
+float faction_hostility(int f1, int f2)
+{
+	if (f1 < 0 || f2 < 0 || f1 >= nfacts || f2 >= nfacts)
+		return 0.0f;
+	return fact[f1].hostility[f2];
 }
 
