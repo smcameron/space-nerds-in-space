bc6c9fec96571cbd252e043b8e9645ea224de777
Jeremy Van Grinsven <jv4779@gmail.com>
2014-02-02 10:44:51 -0600

    change skybox texture filtering to linear for better looking zoom

--- graph_dev_opengl.c
+++ graph_dev_opengl.c
@@ -2328,8 +2328,8 @@ unsigned int graph_dev_load_cubemap_texture(
 	GLuint cube_texture_id;
 	glGenTextures(1, &cube_texture_id);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, cube_texture_id);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
