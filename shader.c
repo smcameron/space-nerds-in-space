57ae1aa17ac7e04ba3f5091e6586f14e2b90f022
Jeremy Van Grinsven <jv4779@gmail.com>
2014-02-18 10:36:27 -0600

    allow building a shader from multiple files

--- shader.c
+++ shader.c
@@ -39,92 +39,159 @@ static char *read_file(const char *filename)
 	return buffer;
 }
 
-GLuint load_shaders(const char *vertex_file_path, const char *fragment_file_path)
+GLuint load_concat_shaders(
+	const char *vertex_header, int nvertex_files, const char *vertex_file_path[],
+	const char *fragment_header, int nfragment_files, const char *fragment_file_path[])
 {
-	/* Create the shaders */
-	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	GLuint program_id = 0;
+	int i;
 
-	/* Read the Vertex Shader code from the file */
-	char *vertexShaderCode = read_file(vertex_file_path);
-	if (!vertexShaderCode) {
-		printf("Can't to open vertex shader '%s'\n", vertex_file_path);
-		return 0;
+	/* Create the shaders */
+	GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+	int nvertex_shader = 0;
+	char *vertex_shader_filename[nvertex_files + 1];
+	GLchar *vertex_shader_code[nvertex_files + 1];
+
+	int nfragment_shader = 0;
+	char *fragment_shader_filename[nfragment_files + 1];
+	GLchar *fragment_shader_code[nfragment_files + 1];
+
+	/* Build the Vertex Shader code */
+	if (vertex_header) {
+		vertex_shader_filename[nvertex_shader] = strdup("string_vertex_header");
+		vertex_shader_code[nvertex_shader] = strdup(vertex_header);
+		nvertex_shader++;
+	}
+
+	for (i = 0; i < nvertex_files; i++) {
+		char *file_shader_code = read_file(vertex_file_path[i]);
+		if (!file_shader_code) {
+			printf("Can't open vertex shader '%s'\n", vertex_file_path[i]);
+			goto cleanup;
+		}
+
+		vertex_shader_filename[nvertex_shader] = strdup(vertex_file_path[i]);
+		vertex_shader_code[nvertex_shader] = file_shader_code;
+		nvertex_shader++;
 	}
 
 	/* Read the Fragment Shader code from the file */
-	char *fragmentShaderCode = read_file(fragment_file_path);
-	if (!fragmentShaderCode) {
-		printf("Can't to open fragment shader '%s'\n", fragment_file_path);
-		free(vertexShaderCode);
-		return 0;
+	if (fragment_header) {
+		fragment_shader_filename[nfragment_shader] = strdup("string_fragment_header");
+		fragment_shader_code[nfragment_shader] = strdup(fragment_header);
+		nfragment_shader++;
+	}
+
+	for (i = 0; i < nfragment_files; i++) {
+		char *file_shader_code = read_file(fragment_file_path[i]);
+		if (!file_shader_code) {
+			printf("Can't open fragment shader '%s'\n", fragment_file_path[i]);
+			goto cleanup;
+		}
+
+		fragment_shader_filename[nfragment_shader] = strdup(fragment_file_path[i]);
+		fragment_shader_code[nfragment_shader] = file_shader_code;
+		nfragment_shader++;
 	}
 
 	GLint result = GL_FALSE;
-	int infoLogLength;
+	int info_log_length;
 
 	/* Compile Vertex Shader */
-	printf("Compiling shader: %s\n", vertex_file_path);
-	char const *vertexSourcePointer = vertexShaderCode;
-	glShaderSource(vertexShaderID, 1, &vertexSourcePointer , NULL);
-	glCompileShader(vertexShaderID);
+	glShaderSource(vertex_shader_id, nvertex_shader, (const GLchar **)&vertex_shader_code[0], NULL);
+	glCompileShader(vertex_shader_id);
 
 	/* Check Vertex Shader */
-	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &result);
 	if (result == GL_FALSE) {
-		glGetShaderiv(vertexShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
-		if (infoLogLength > 0) {
-			char *vertexShaderErrorMessage = malloc(infoLogLength+1);
-			glGetShaderInfoLog(vertexShaderID, infoLogLength, NULL, vertexShaderErrorMessage);
-			printf("Shader '%s' compile error: %s\n", vertex_file_path, vertexShaderErrorMessage);
-			free(vertexShaderErrorMessage);
+		printf("Vertex shader compile error in files:\n");
+		for (i = 0; i < nvertex_shader; i++) {
+			printf("    %d: \"%s\"\n", i, vertex_shader_filename[i]);
 		}
+		glGetShaderiv(vertex_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+		if (info_log_length > 0) {
+			GLchar error_message[info_log_length + 1];
+			glGetShaderInfoLog(vertex_shader_id, info_log_length, NULL, &error_message[0]);
+			printf("Error:\n%s\n", error_message);
+		} else {
+			printf("error: unknown\n");
+		}
+		goto cleanup;
 	}
 
 	/* Compile Fragment Shader */
-	printf("Compiling shader : %s\n", fragment_file_path);
-	char const *fragmentSourcePointer = fragmentShaderCode;
-	glShaderSource(fragmentShaderID, 1, &fragmentSourcePointer , NULL);
-	glCompileShader(fragmentShaderID);
+	glShaderSource(fragment_shader_id, nfragment_shader, (const GLchar **)&fragment_shader_code[0], NULL);
+	glCompileShader(fragment_shader_id);
 
 	/* Check Fragment Shader */
-	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &result);
 	if (result == GL_FALSE) {
-		glGetShaderiv(fragmentShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
-		if (infoLogLength > 0) {
-			char *fragmentShaderErrorMessage = malloc(infoLogLength+1);
-			glGetShaderInfoLog(fragmentShaderID, infoLogLength, NULL, fragmentShaderErrorMessage);
-			printf("Shader '%s' compile error: %s\n", fragment_file_path, fragmentShaderErrorMessage);
-			free(fragmentShaderErrorMessage);
+		printf("Fragment shader compile error in files:\n");
+		for (i = 0; i < nfragment_shader; i++) {
+			printf("    %d: \"%s\"\n", i, fragment_shader_filename[i]);
 		}
+		glGetShaderiv(fragment_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+		if (info_log_length > 0) {
+			char error_message[info_log_length + 1];
+			glGetShaderInfoLog(fragment_shader_id, info_log_length, NULL, &error_message[0]);
+			printf("Error:\n%s\n", error_message);
+		} else {
+			printf("error: unknown\n");
+		}
+		goto cleanup;
 	}
 
 	/* Link the program */
-	printf("Linking program\n");
-	GLuint programID = glCreateProgram();
-	glAttachShader(programID, vertexShaderID);
-	glAttachShader(programID, fragmentShaderID);
-	glLinkProgram(programID);
+	program_id = glCreateProgram();
+	glAttachShader(program_id, vertex_shader_id);
+	glAttachShader(program_id, fragment_shader_id);
+	glLinkProgram(program_id);
 
 	/* Check the program */
-	glGetProgramiv(programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(program_id, GL_LINK_STATUS, &result);
 	if (result == GL_FALSE) {
-		glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
-		if (infoLogLength > 0) {
-			char *programErrorMessage = malloc(infoLogLength+1);
-			glGetProgramInfoLog(programID, infoLogLength, NULL, programErrorMessage);
-			printf("Shader link error: %s\n", programErrorMessage);
-			free(programErrorMessage);
+		printf("Shader link error in files:\n");
+		for (i = 0; i < nvertex_shader; i++) {
+			printf("    vert %d: \"%s\"\n", i, vertex_shader_filename[i]);
 		}
+		for (i = 0; i < nfragment_shader; i++) {
+			printf("    frag %d: \"%s\"\n", i, fragment_shader_filename[i]);
+		}
+
+		glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &info_log_length);
+		if (info_log_length > 0) {
+			GLchar error_message[info_log_length + 1];
+			glGetProgramInfoLog(program_id, info_log_length, NULL, &error_message[0]);
+			printf("Error:\n%s\n", error_message);
+
+			glDeleteProgram(program_id);
+			program_id = 0;
+		} else {
+			printf("error: unknown\n");
+		}
+		goto cleanup;
 	}
 
-	glDeleteShader(vertexShaderID);
-	glDeleteShader(fragmentShaderID);
+cleanup:
+	glDeleteShader(vertex_shader_id);
+	glDeleteShader(fragment_shader_id);
 
-	free(vertexShaderCode);
-	free(fragmentShaderCode);
+	for (i = 0; i < nvertex_shader; i++) {
+		free(vertex_shader_filename[i]);
+		free(vertex_shader_code[i]);
+	}
+	for (i = 0; i < nfragment_shader; i++) {
+		free(fragment_shader_filename[i]);
+		free(fragment_shader_code[i]);
+	}
 
-	return programID;
+	return program_id;
 }
 
+GLuint load_shaders(const char *vertex_file_path, const char *fragment_file_path)
+{
+	return load_concat_shaders(0, 1, &vertex_file_path, 0, 1, &fragment_file_path);
+}
 
