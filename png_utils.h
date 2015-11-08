2869af6e4268b076863821e26a97453cdd0a777f
Stephen M. Cameron <stephenmcameron@gmail.com>
2015-11-08 09:52:04 -0800

    Factor out read_png_image to png_utils
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- png_utils.h
+++ png_utils.h
@@ -23,4 +23,9 @@
 #include <png.h>
 
 int png_utils_write_png_image(const char *filename, unsigned char *pixels, int w, int h, int has_alpha, int invert);
+
+char *png_utils_read_png_image(const char *filename, int flipVertical, int flipHorizontal,
+        int pre_multiply_alpha,
+        int *w, int *h, int *hasAlpha, char *whynot, int whynotlen);
+
 #endif
