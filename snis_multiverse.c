dc79170143d2ec7d774a5b99877de67c3b8e0378
Stephen M. Cameron <stephenmcameron@gmail.com>
2016-02-05 20:16:39 -0800

    Transmit player ship power model data to multiverse server
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_multiverse.c
+++ snis_multiverse.c
@@ -49,6 +49,7 @@ persisted in a simple database by snis_multiverse.
 #include <limits.h>
 #include <dirent.h>
 
+#include "build_bug_on.h"
 #include "snis_marshal.h"
 #include "quat.h"
 #include "ssgl/ssgl.h"
@@ -271,7 +272,51 @@ struct key_value_specification snis_entity_kvs[] = {
 	/* TODO damcon data... */
 	UINT8_TSDFIELD(view_mode),
 	DOUBLE_TSDFIELD(view_angle),
-	/* TODO power_data */
+
+	/* Beginning of power_data */
+	UINT8_TSDFIELD(power_data.maneuvering.r1),
+	UINT8_TSDFIELD(power_data.maneuvering.r2),
+	UINT8_TSDFIELD(power_data.maneuvering.r3),
+	UINT8_TSDFIELD(power_data.maneuvering.i),
+
+	UINT8_TSDFIELD(power_data.warp.r1),
+	UINT8_TSDFIELD(power_data.warp.r2),
+	UINT8_TSDFIELD(power_data.warp.r3),
+	UINT8_TSDFIELD(power_data.warp.i),
+
+	UINT8_TSDFIELD(power_data.impulse.r1),
+	UINT8_TSDFIELD(power_data.impulse.r2),
+	UINT8_TSDFIELD(power_data.impulse.r3),
+	UINT8_TSDFIELD(power_data.impulse.i),
+
+	UINT8_TSDFIELD(power_data.sensors.r1),
+	UINT8_TSDFIELD(power_data.sensors.r2),
+	UINT8_TSDFIELD(power_data.sensors.r3),
+	UINT8_TSDFIELD(power_data.sensors.i),
+
+	UINT8_TSDFIELD(power_data.comms.r1),
+	UINT8_TSDFIELD(power_data.comms.r2),
+	UINT8_TSDFIELD(power_data.comms.r3),
+	UINT8_TSDFIELD(power_data.comms.i),
+
+	UINT8_TSDFIELD(power_data.phasers.r1),
+	UINT8_TSDFIELD(power_data.phasers.r2),
+	UINT8_TSDFIELD(power_data.phasers.r3),
+	UINT8_TSDFIELD(power_data.phasers.i),
+
+	UINT8_TSDFIELD(power_data.shields.r1),
+	UINT8_TSDFIELD(power_data.shields.r2),
+	UINT8_TSDFIELD(power_data.shields.r3),
+	UINT8_TSDFIELD(power_data.shields.i),
+
+	UINT8_TSDFIELD(power_data.tractor.r1),
+	UINT8_TSDFIELD(power_data.tractor.r2),
+	UINT8_TSDFIELD(power_data.tractor.r3),
+	UINT8_TSDFIELD(power_data.tractor.i),
+
+	UINT8_TSDFIELD(power_data.voltage),
+	/* End of power_data */
+
 	/* TODO power_model */
 	/* TODO coolant_data */
 	/* TODO coolant_model */
@@ -544,7 +589,7 @@ static int update_bridge(struct starsystem_info *ss)
 {
 	unsigned char pwdhash[20];
 	int i, rc;
-	unsigned char buffer[200], name[20];
+	unsigned char buffer[200];
 	struct packed_buffer pb;
 	uint16_t alive;
 	uint32_t torpedoes, power;
@@ -559,15 +604,21 @@ static int update_bridge(struct starsystem_info *ss)
 	union quat orientation, sciball_orientation, weap_orientation;
 	union euler ypr;
 	struct snis_entity *o;
+	unsigned char name[sizeof(o->sdata.name)];
 	int32_t iwallet;
+	struct power_model_data power_data;
+#define bytes_to_read (sizeof(struct update_ship_packet) - 9 + 25 + 5 + sizeof(struct power_model_data) - 1)
 
 	fprintf(stderr, "snis_multiverse: update bridge 1\n");
+	memset(buffer, 0, sizeof(buffer));
+	memset(pwdhash, 0, sizeof(pwdhash));
 	rc = read_and_unpack_fixed_size_buffer(ss, buffer, 20, "r", pwdhash, (uint16_t) 20);
 	if (rc != 0)
 		return rc;
-	print_hash("update bridge 2: ", pwdhash);
-	assert(sizeof(buffer) > sizeof(struct update_ship_packet) - 9);
-	rc = snis_readsocket(ss->socket, buffer, sizeof(struct update_ship_packet) - 9 + 25);
+	print_hash("update bridge 2, read 20 bytes: ", pwdhash);
+	BUILD_ASSERT(sizeof(buffer) > bytes_to_read);
+	memset(buffer, 0, sizeof(buffer));
+	rc = snis_readsocket(ss->socket, buffer, bytes_to_read);
 	if (rc != 0)
 		return rc;
 	fprintf(stderr, "snis_multiverse: update bridge 3\n");
@@ -579,7 +630,7 @@ static int update_bridge(struct starsystem_info *ss)
 		return rc;
 	}
 	fprintf(stderr, "snis_multiverse: update bridge 4\n");
-	packed_buffer_init(&pb, buffer, sizeof(buffer));
+	packed_buffer_init(&pb, buffer, bytes_to_read);
 	packed_buffer_extract(&pb, "hSSS", &alive,
 				&dx, (int32_t) UNIVERSE_DIM, &dy, (int32_t) UNIVERSE_DIM,
 				&dz, (int32_t) UNIVERSE_DIM);
@@ -603,7 +654,8 @@ static int update_bridge(struct starsystem_info *ss)
 			&docking_magnets, (uint32_t *) &iwallet);
 	fprintf(stderr, "snis_multiverse: update bridge 7\n");
 	packed_buffer_extract(&pb, "bbbbbr", &shield_strength, &shield_wavelength, &shield_width, &shield_depth,
-			&faction, name, (int) sizeof(name));
+			&faction, name, (uint16_t) sizeof(name));
+	packed_buffer_extract(&pb, "r", &power_data, (uint16_t) sizeof(struct power_model_data));
 	tloaded = (tloading >> 4) & 0x0f;
 	tloading = tloading & 0x0f;
 	quat_to_euler(&ypr, &orientation);
@@ -667,6 +719,7 @@ static int update_bridge(struct starsystem_info *ss)
 	o->sdata.shield_depth = shield_depth;
 	o->sdata.faction = faction;
 	memcpy(o->sdata.name, name, sizeof(o->sdata.name));
+	o->tsd.ship.power_data = power_data;
 
 	/* shift old updates to make room for this one */
 	int j;
