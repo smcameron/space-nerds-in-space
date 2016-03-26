1bc32c1b7b5dfcf26438245a6b01eb240e7c75de
Stephen M. Cameron <stephenmcameron@gmail.com>
2016-03-26 08:38:51 -0700

    Distinguish appearance of buttons from textboxes by rounding button corners
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_button.c
+++ snis_button.c
@@ -47,6 +47,27 @@ struct button *snis_button_init(int x, int y, int width, int height,
 	return b;
 }
 
+static void snis_button_draw_outline(float x1, float y1, float width, float height)
+{
+	float dx = width * 0.03;
+	float dy = height * 0.15;
+	float d;
+	if (dx < dy)
+		d = dx;
+	else
+		d = dy;
+
+	sng_current_draw_line(x1 + d, y1, x1 + width - d, y1);
+	sng_current_draw_line(x1 + d, y1 + height, x1 + width - d, y1 + height);
+	sng_current_draw_line(x1, y1 + d, x1, y1 + height - d);
+	sng_current_draw_line(x1 + width, y1 + d, x1 + width, y1 + height - d);
+
+	sng_current_draw_line(x1 + d, y1, x1, y1 + d);
+	sng_current_draw_line(x1 + width - d, y1, x1 + width, y1 + d);
+	sng_current_draw_line(x1, y1 + height - d, x1 + d, y1 + height);
+	sng_current_draw_line(x1 + width, y1 + height - d, x1 + width - d, y1 + height);
+}
+
 void snis_button_draw(struct button *b)
 {
 	int offset;
@@ -56,20 +77,27 @@ void snis_button_draw(struct button *b)
 		offset = 1;
 	else
 		offset = 0;
-	sng_current_draw_rectangle(0, b->x + offset, b->y + offset,
+	snis_button_draw_outline(b->x + offset, b->y + offset,
 					b->width + offset, b->height + offset);
 	if (b->button_press_feedback_counter)
-		sng_current_draw_rectangle(0, b->x + 1 + offset, b->y + 1 + offset,
+		snis_button_draw_outline(b->x + 1 + offset, b->y + 1 + offset,
 					b->width - 2 + offset, b->height - 2 + offset);
 	if (!b->checkbox_value) {
 		sng_abs_xy_draw_string(b->label, b->font, b->x + 10, b->y + b->height / 1.7);
 		if (b->button_press_feedback_counter)
 			sng_abs_xy_draw_string(b->label, b->font, b->x + 11, b->y + b->height / 1.7 + 1);
 	} else {
-		sng_current_draw_rectangle(0, b->x + 5, b->y + 2, 15, 15);
+		float x1, y1, x2, y2;
+
+		x1 = b->x + 5;
+		x2 = b->x + 5 + 16;
+		y1 = b->y + b->height / 2 - 8;
+		y2 = b->y + b->height / 2 + 8;
+
+		sng_current_draw_rectangle(0, x1, y1, 16, 16);
 		if (*b->checkbox_value) {
-			sng_current_draw_line(b->x + 5, b->y + 2, b->x + 5 + 15, b->y + 2 + 15);
-			sng_current_draw_line(b->x + 5, b->y + 2 + 15, b->x + 5 + 15, b->y + 2);
+			sng_current_draw_line(x1, y1, x2, y2);
+			sng_current_draw_line(x1, y2, x2, y1);
 		}
 		sng_abs_xy_draw_string(b->label, b->font, b->x + 30, b->y + b->height / 1.7);
 		if (b->button_press_feedback_counter)
