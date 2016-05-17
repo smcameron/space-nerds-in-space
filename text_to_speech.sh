523bef9db79696e2c477b45a32a68ef1dbe93b8a
Stephen M. Cameron <stephenmcameron@gmail.com>
2016-05-16 21:05:10 -0700

    Serialize text to speech to prevent the computer from talking over itself
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- text_to_speech.sh
+++ text_to_speech.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
+export SNISTTSLOCKDIR=/tmp/snisttslockdir
+
+release_lock()
+{
+	/bin/rmdir "$SNISTTSLOCKDIR"
+}
+
+
+get_lock()
+{
+	count=0
+	worked=0
+	while [ $count -lt 20 ]
+	do
+		mkdir "$SNISTTSLOCKDIR" > /dev/null 2>&1
+		if [ "$?" = "0" ]
+		then
+			trap release_lock INT EXIT
+			worked=1;
+			break;
+		fi
+		count=$(expr $count + 1)
+		sleep 1
+	done
+	if [ "$worked" != "1" ]
+	then
+		exit 1
+	fi
+}
+
 do_text_to_speech()
 {
+	get_lock
 	tmpfile=/tmp/tts-$$.wav
 	pico2wave -l=en-GB -w "$tmpfile" "$1" || espeak "$1"
 	aplay "$tmpfile" > /dev/null 2>&1
