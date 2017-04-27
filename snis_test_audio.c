ab6ab4fcdfe567aa3494f9c111967b1fee90f8e0
Stephen M. Cameron <stephenmcameron@gmail.com>
2017-04-27 07:23:23 -0700

    Improve output of snis_test_audio
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_test_audio.c
+++ snis_test_audio.c
@@ -13,6 +13,14 @@ int main(int argc, char *argv[])
 	char *sound_file = "share/snis/sounds/red-alert.ogg";
 
 	printf("Space Nerds In Space audio test program.\n");
+	printf("How to use this program:\n");
+	printf("Type the following:\n");
+	printf("  make snis_test_audio\n");
+	printf("  ./snis_test_audio\n\n");
+	printf("Or, if you want to try a sound device other than the default, such as device 6:\n\n");
+	printf("  ./snis_test_audio 6\n\n");
+	printf("The device numbers should be listed below.\n\n\n");
+
 	if (argc > 1) {
 		sound_device_str = argv[1];
 		rc = sscanf(sound_device_str, "%d", &sound_device);
@@ -30,6 +38,7 @@ int main(int argc, char *argv[])
 	if (rc != 0) {
 		return rc;
 	}
+	sound_device = wwviaudio_get_sound_device();
 
 	rc = wwviaudio_read_ogg_clip(0, sound_file);
 	if (rc != 0) {
@@ -39,7 +48,8 @@ int main(int argc, char *argv[])
 
 	wwviaudio_list_devices();
 
-	printf("Attempting to play sound... ");
+	printf("Attempting to play sound '%s' to device %d... ", sound_file, sound_device);
+	fflush(stdout);
 	wwviaudio_add_sound(0);
 	sleep(1);
 	printf("...finished attempting to play sound\n");
