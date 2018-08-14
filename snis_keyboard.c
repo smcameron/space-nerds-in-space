42eab179a875b9d9d4c12e7b3b4f28947a6d2336
Stephen M. Cameron <stephenmcameron@gmail.com>
2018-08-14 08:26:17 -0400

    Suppress WSD keys on science as they interfere with AWSD on NAV, WEAPONS, and MAINSCREEN
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_keyboard.c
+++ snis_keyboard.c
@@ -255,10 +255,17 @@ void init_keymap()
 	keymap[GDK_KEY_minus] = keyunzoom;
 	keymap[GDK_KEY_m] = key_sci_mining_bot;
 	keymap[GDK_KEY_t] = key_sci_tractor_beam;
-	keymap[GDK_KEY_l] = key_sci_lrs;
-	keymap[GDK_KEY_s] = key_sci_srs;
-	keymap[GDK_KEY_w] = key_sci_waypoints;
-	keymap[GDK_KEY_d] = key_sci_details;
+
+	/* keymap[GDK_KEY_l] = key_sci_lrs; */ /* <-- this interferes with keysciball_pitchdown, above */
+
+	/* NOTE: The following 3 keys interfere with AWSD on Nav, Weapons, and Mainscreen.
+	 * The real fix is to have per-screen keybindings, not game-wide keybindings.
+	 */
+
+	/* keymap[GDK_KEY_s] = key_sci_srs; */
+	/* keymap[GDK_KEY_w] = key_sci_waypoints; */
+	/* keymap[GDK_KEY_d] = key_sci_details; */
+
 	ffkeymap[GDK_KEY_KP_Add & 0x00ff] = keyzoom;
 	ffkeymap[GDK_KEY_KP_Subtract & 0x00ff] = keyunzoom;
 
