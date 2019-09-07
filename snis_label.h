313a73dd8edd5934aaf79c198644d398cb0c2ea0
Stephen M. Cameron <stephenmcameron@gmail.com>
2019-09-07 09:32:44 -0400

    Document snis_label better
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_label.h
+++ snis_label.h
@@ -9,11 +9,16 @@
 
 struct label;
 
+/* Returns a new label.
+ * x, y: position of label.
+ * label: value of label, e.g.: "RPM"
+ * color: color of label
+ * font: font of label
+ */
 GLOBAL struct label *snis_label_init(int x, int y, char *label,
 					int color, int font);
-GLOBAL void snis_label_draw(struct label *l);
-
-GLOBAL void snis_label_set_color(struct label *l, int color);
+GLOBAL void snis_label_draw(struct label *l); /* draw the label */
+GLOBAL void snis_label_set_color(struct label *l, int color); /* Set the color of a label */
 
 #undef GLOBAL
 #endif
