smcameron/space-nerds-in-space

commit def78e49acd70e5b7a632d91011bd289cf46f6e3

Update makefile with new lua scripts
Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

Makefile
@@ -270,14 +270,16 @@ LUASRCDIR=${ASSETSSRCDIR}/luascripts
 LUASCRIPTS=${LUASRCDIR}/MAINMENU.LUA \
 	${LUASRCDIR}/initialize.lua
 LUAMISSIONS=${LUASRCDIR}/MISSIONS/BIGSHIP.LUA \
+	${LUASRCDIR}/MISSIONS/CUBOMUERTE.LUA \
 	${LUASRCDIR}/MISSIONS/DEMOLISHER.LUA \
 	${LUASRCDIR}/MISSIONS/KALI.LUA \
+	${LUASRCDIR}/MISSIONS/lib/interactive-fiction.lua \
 	${LUASRCDIR}/MISSIONS/ROYAL-WEDDING.LUA \
 	${LUASRCDIR}/MISSIONS/SAVING-PLANET-ERPH.LUA \
 	${LUASRCDIR}/MISSIONS/SPACEPOX.LUA \
+	${LUASRCDIR}/MISSIONS/STURNVULF.LUA \
 	${LUASRCDIR}/MISSIONS/TRAINING-MISSION-1.LUA \
-	${LUASRCDIR}/MISSIONS/TRAINING-MISSION-2.LUA \
-	${LUASRCDIR}/MISSIONS/lib/interactive_fiction.lua
+	${LUASRCDIR}/MISSIONS/TRAINING-MISSION-2.LUA
 LUATESTS=${LUASRCDIR}/TEST/CARGO.LUA \
 	${LUASRCDIR}/TEST/COLLISION.LUA \
 	${LUASRCDIR}/TEST/REVIEW_MODELS.LUA \
@@ -289,14 +291,17 @@ LUATESTS=${LUASRCDIR}/TEST/CARGO.LUA \
 	${LUASRCDIR}/TEST/TESTEXPL.LUA \
 	${LUASRCDIR}/TEST/TESTSPHEROID.LUA \
 	${LUASRCDIR}/TEST/TESTWARPEVENT.LUA \
-	${LUASRCDIR}/TEST/TESTINTFIC.LUA
+	${LUASRCDIR}/TEST/TESTINTFIC.LUA \
+	${LUASRCDIR}/TEST/TESTRUNAWAY.LUA
 LUAUTILS=${LUASRCDIR}/UTIL/CLEAR_ALL.LUA \
 	${LUASRCDIR}/UTIL/DAMAGE.LUA \
+	${LUASRCDIR}/UTIL/DESTROY_SHIP.LUA \
 	${LUASRCDIR}/UTIL/DISABLE_ANTENNA.LUA \
 	${LUASRCDIR}/UTIL/ENABLE_ANTENNA.LUA \
 	${LUASRCDIR}/UTIL/PAY_PLAYER.LUA \
 	${LUASRCDIR}/UTIL/PLAYER_SPEED_BOOST.LUA \
 	${LUASRCDIR}/UTIL/PLAYER_SPEED_DEFAULT.LUA \
+	${LUASRCDIR}/UTIL/REDALERT.LUA \
 	${LUASRCDIR}/UTIL/REGENERATE.LUA \
 	${LUASRCDIR}/UTIL/RESETBRIDGE.LUA \
 	${LUASRCDIR}/UTIL/RESTORE_SKYBOX.LUA \

commit 3c4fd2e59c8da684aec57bae38f07ab506a64f35

Makefile: fix lua file path
Commit def78e49acd70e5b7a632d91011bd289cf46f6e3 introduced a typo,
changing `interactive_fiction.lua` to `interactive-fiction.lua`. This
commit corrects the typo, fixing `make install`

Signed-off-by: Andy Boyett <agb@agb.io>

Makefile
@@ -272,7 +272,7 @@ LUAMISSIONS=${LUASRCDIR}/MISSIONS/BIGSHIP.LUA \
 	${LUASRCDIR}/MISSIONS/CUBOMUERTE.LUA \
 	${LUASRCDIR}/MISSIONS/DEMOLISHER.LUA \
 	${LUASRCDIR}/MISSIONS/KALI.LUA \
-	${LUASRCDIR}/MISSIONS/lib/interactive-fiction.lua \
+	${LUASRCDIR}/MISSIONS/lib/interactive_fiction.lua \
 	${LUASRCDIR}/MISSIONS/ROYAL-WEDDING.LUA \
 	${LUASRCDIR}/MISSIONS/SAVING-PLANET-ERPH.LUA \
 	${LUASRCDIR}/MISSIONS/SPACEPOX.LUA \