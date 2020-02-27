a75a337b97672a3d390235957becbad12d74b89b
Stephen M. Cameron <stephenmcameron@gmail.com>
2020-02-27 13:12:58 -0500

    Fix update ship opcode definition
    Somehow it is out of sync with what is really transmitted.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_opcode_def.c
+++ snis_opcode_def.c
@@ -69,7 +69,7 @@ int snis_opcode_def_init(void)
 {
 	int rc = 0;
 
-	rc |= init_opcode_def(OPCODE_UPDATE_SHIP, "bwwhSSSRRRwwwRRRbbbwbbbbbbbbbbbbbbwQQQQSSSbbbbbbbbbbww");
+	rc |= init_opcode_def(OPCODE_UPDATE_SHIP, "bwwhSSSRRRwwRRbbbwwbbbbbbbbbbbbwQQQQSSSbB8bbww");
 	rc |= init_opcode_def(OPCODE_UPDATE_STARBASE, "bwwSSSQbbbbwbb");
 	rc |= init_opcode_def(OPCODE_UPDATE_LASER, "bwwwbSSSQ");
 	rc |= init_opcode_def(OPCODE_UPDATE_TORPEDO, "bwwwSSS");
