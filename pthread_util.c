b453e3290dd43a3efe842eefbc8fc07ced0f36c9
Stephen M. Cameron <stephenmcameron@gmail.com>
2020-04-11 08:25:50 -0400

    Unify the thread creation to always use the trampoline function.
    Apple's pthread_setname_np() has a different calling convention and
    a thread can only set its own name whereas on linux, a thread can
    set any name and has a thread parameter to identify which thread.
    We can adapt the linux code to work in much the same way as the
    OSX code and reduce the amount of code under #ifdef to just the
    pthread_setname_np() call.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- pthread_util.c
+++ pthread_util.c
@@ -20,72 +20,66 @@
 */
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
 
 #include "pthread_util.h"
 
-struct pthread_osx_trampoline_args {
+struct pthread_setname_trampoline_args {
 	const char *thread_name;
 	void *(*thread_start) (void *);
 	void *thread_args;
+	pthread_t *thread;
 };
 
-#ifdef __APPLE__
-void *pthread_osx_trampoline(void *args)
+/* On MacOS X we can only set a threads name from inside that thread.
+ * Because of this, we have pthread_create specify a trampoline function
+ * that calls the actual routine that the thread will be running.
+ */
+void *pthread_setname_trampoline(void *args)
 {
-	printf("Trampoline executed!\n");
-	struct pthread_osx_trampoline_args *t_args = args;
+	struct pthread_setname_trampoline_args *t_args = args;
 	const char *thread_name = t_args->thread_name;
 	void *thread_args = t_args->thread_args;
+	void *rc;
 
+#ifdef __APPLE__
 	pthread_setname_np(thread_name);
-	printf("Thread name: %s\n", t_args->thread_name);
-	t_args->thread_start(thread_args);
-	free(args);
-}
+#else
+#ifdef _GNU_SOURCE
+	pthread_setname_np(*t_args->thread, thread_name);
 #endif
+#endif
+	rc = t_args->thread_start(thread_args);
+	free(args);
+	return rc;
+}
 
 int create_thread(pthread_t *thread,  void *(*start_routine) (void *), void *arg, char *name, int detached)
 {
 	int rc;
 	pthread_attr_t attr;
 	char *thread_name = NULL;
-#ifdef __APPLE__
-	struct pthread_osx_trampoline_args *t_args;
-#endif
+	struct pthread_setname_trampoline_args *t_args;
 
 	if (name) {
 		thread_name = strdup(name);
 		if (strlen(thread_name) >= 15)
 			thread_name[15] = '\0';
 	}
+	t_args = malloc(sizeof(struct pthread_setname_trampoline_args));
+	t_args->thread_name = thread_name;
+	t_args->thread_start = start_routine;
+	t_args->thread_args = arg;
+	t_args->thread = thread;
 	if (detached) {
 		pthread_attr_init(&attr);
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-		/* On MacOS X we can only set a threads name from inside that thread.
-		 * Because of this, if we're compiling for OS X, we have pthread_create
-		 * specify a trampoline function that calls the actual routine that the
-		 * thread will be running.
-		 */
-#ifndef __APPLE__
-		rc = pthread_create(thread, &attr, start_routine, arg);
-#else
-		t_args = malloc(sizeof(struct pthread_osx_trampoline_args));
-		t_args->thread_name = name;
-		t_args->thread_start = start_routine;
-		t_args->thread_args = arg;
-		rc = pthread_create(thread, &attr, pthread_osx_trampoline, (void *) t_args);
-#endif
+		rc = pthread_create(thread, &attr, pthread_setname_trampoline, t_args);
 		pthread_attr_destroy(&attr);
 	} else {
-		rc = pthread_create(thread, NULL, start_routine, arg);
+		rc = pthread_create(thread, NULL, pthread_setname_trampoline, t_args);
 	}
-#ifdef _GNU_SOURCE
-#ifndef __APPLE__
-	if (!rc && name && thread_name)
-		pthread_setname_np(*thread, thread_name);
-#endif
-#endif
 	return rc;
 }
