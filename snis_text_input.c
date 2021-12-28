c7a7b49fc659c31ab9724a3612bf50ae9df09648
Stephen M. Cameron <stephenmcameron@gmail.com>
2021-12-28 11:38:15 -0500

    snis_text_input: fix signed/unsigned compare in do_rightarrow()
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_text_input.c
+++ snis_text_input.c
@@ -163,7 +163,7 @@ static void do_delete(struct snis_text_input_box *t)
 
 static void do_rightarrow(struct snis_text_input_box *t)
 {
-	if (t->cursor_pos < t->buflen && t->cursor_pos < strlen(t->buffer))
+	if (t->cursor_pos < t->buflen && (size_t) t->cursor_pos < strlen(t->buffer))
 		t->cursor_pos++;
 }
 
