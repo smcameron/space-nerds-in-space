e1fa2ba23b29adfa0a78a0e73e9a2c61a4dce973
Stephen M. Cameron <stephenmcameron@gmail.com>
2021-12-29 08:13:21 -0500

    test-obj-parser: make it a little more robust
    Add usage message, rudimentary checks on argc, argv[]
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- test-obj-parser.c
+++ test-obj-parser.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #include "mtwist.h"
 #include "quat.h"
@@ -24,12 +27,25 @@ void mesh_graph_dev_cleanup(struct mesh *m)
 {
 }
 
+static void usage(void)
+{
+	fprintf(stderr, "usage: test-obj-parser somefile.obj\n");
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
 	struct mesh *m;
 
+	if (argc < 2)
+		usage();
+
+
 	m = read_obj_file(argv[1]);
 	printf("m = %p\n", m);
+	if (!m) {
+		fprintf(stderr, "errno = %d: %s\n", errno, strerror(errno));
+	}
 
 	return 0;
 }
