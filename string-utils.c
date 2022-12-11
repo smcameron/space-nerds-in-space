7fc25703762d21e5efa86e0e36b0fcd004c4bfe2
Stephen M. Cameron <stephenmcameron@gmail.com>
2022-12-11 13:20:28 -0500

    Make custom strlcpy function return correct value
    It's supposed to return strlen(src), not strlen(dest).
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- string-utils.c
+++ string-utils.c
@@ -197,26 +197,22 @@ int strchrcount(char *s, int c)
 #if USE_CUSTOM_STRLCPY
 size_t strlcpy(char *dest, const char *src, size_t n)
 {
-	size_t rc;
-	int i;
+	size_t i;
 
 	for (i = 0;; i++) {
 		if (i < n)
 			dest[i] = src[i];
 		else {
 			dest[n - 1] = '\0';
-			if (src[i])
-				rc = i + 1 + strlen(&src[i + 1]);
-			else
-				rc = i;
 			break;
 		}
-		if (!src[i]) {
-			rc = i;
-			break;
-		}
+		if (src[i] == '\0')
+			return i;
 	}
-	return rc;
+	for (;; i++)
+		if (src[i] == '\0')
+			break;
+	return i;
 }
 #endif
 
