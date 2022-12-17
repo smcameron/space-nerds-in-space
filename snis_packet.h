3d550e6cd5ecb10f2cb1dbf73abe7fb451b4a828
Stephen M. Cameron <stephenmcameron@gmail.com>
2022-12-17 08:08:16 -0500

    Fix minor error in snis_packet.h
    missile_lock_detected is actually in the flag_bits field,
    and comms_crypto_mode was missing.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- snis_packet.h
+++ snis_packet.h
@@ -318,9 +318,10 @@ struct update_ship_packet {
 	uint8_t rts_mode;
 	uint8_t exterior_lights;
 	uint8_t alarms_silenced;
+	uint8_t missile_lock_detected;
 #endif
 	uint8_t flag_bits;
-	uint8_t missile_lock_detected;
+	uint8_t comms_crypto_mode;
 	uint8_t rts_active_button;
 	uint32_t wallet;
 	uint32_t viewpoint_object;
