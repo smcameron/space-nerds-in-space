359147befec023fa0257d00df57a6e198cd9bacd
Stephen M. Cameron <stephenmcameron@gmail.com>
2024-01-26 09:34:37 -0500

    science: make gas giant mass more believable
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- planetary_properties.c
+++ planetary_properties.c
@@ -9,10 +9,13 @@
 static const double planet_mass[] =	{ 0.0, 0.060, 0.105, 1.0, 4.5, 10.0, 40.0, 200.0, };
 static const double planet_radius[] =	{ 0.0, 0.400, 0.550, 1.0, 2.0, 4.00, 6.00, 15.00, };
 
+#define GAS_GIANT_MASS_MULTIPLIER 300.0
+#define GAS_GIANT_RADIUS_MULTIPLIER 12.0
+
 double planetary_mass(double radius, enum planet_type t)
 {
 	double eradius = (radius / 6000.0);
-	double gas_giant_multiplier = (t == planet_type_gas_giant) ? 12.0 : 1.0;
+	double gas_giant_multiplier = (t == planet_type_gas_giant) ? GAS_GIANT_MASS_MULTIPLIER : 1.0;
 
 	if (eradius < 0)
 		eradius = 0;
@@ -26,7 +29,7 @@ double planetary_mass(double radius, enum planet_type t)
 double planetary_diameter(double radius, enum planet_type t)
 {
 	/* We lie about the gas giant diameter to make them seem bigger. */
-	double gas_giant_multiplier = (t == planet_type_gas_giant) ? 12.0 : 1.0;
+	double gas_giant_multiplier = (t == planet_type_gas_giant) ? GAS_GIANT_RADIUS_MULTIPLIER : 1.0;
 	return gas_giant_multiplier * radius / 6000.0;
 }
 
